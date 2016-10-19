3ec6d1224e9bb25d4abd91b5a69bb0a5d9f42c03
Monty Anderson <monty.linux@gmail.com>
2016-10-19 22:56:49 +0100

    total rubbish

--- generateDataset.js
+++ generateDataset.js
@@ -5,6 +5,98 @@ const querystring = require("querystring");
 const got = require("got");
 const CSVToArray = require("./lib/CSVToArray");
 
+const dataRoot = __dirname + "/data/";
+
+const normal = arr => Math.max(...arr);
+const values = obj => Object.keys(obj).map(key => obj[key]);
+
+fs.readdirAsync(dataRoot)
+.then(files => {
+	return Promise.all(files.map(getTrend))
+	.then(trends => {
+		const dataset = {
+			labels: {
+				input: [],
+				output: []
+			},
+			normals: {
+				input: [],
+				output: []
+			},
+			records: []
+		};
+
+		const { normals, records, labels } = dataset;
+
+		const trendMax = 100;
+
+		files.forEach(f => labels.input.push(f));
+
+		for(let key in trends[0]) {
+			const record = { input: [], date: key };
+
+			const inputs = trends.forEach((trend, i) => {
+				record.input[i] = trend[key] / trendMax;
+			});
+
+			if(Object.keys(record.input).every(a => record.input[a] > -1))
+				records.push(record);
+		}
+
+		trends.forEach((trend, i) => {
+			normals.input[i] = trendMax;
+		});
+
+		return dataset;
+	}).then(dataset => {
+		const { normals, records, labels } = dataset;
+
+		labels.output.push("price");
+
+		const query = querystring.stringify({
+			start: records[0].date,
+			end: records[records.length - 1].date
+		});
+
+		const url = "https://api.coindesk.com/v1/bpi/historical/close.json?" + query;
+
+
+		return got(url)
+			.then(response => {
+				const bpi = JSON.parse(response.body).bpi;
+				const n = normal(values(bpi));
+				normals.output[0] = n;
+
+				records.forEach(record => {
+					record.output = [ bpi[record.date] / n ];
+				});
+
+				return dataset;
+			});
+
+		return dataset;
+	}).then(dataset => {
+		fs.writeFileAsync(__dirname + "/dataset.json", JSON.stringify(dataset, null, "\t"));
+	});
+});
+
+function getTrend(f) {
+	const uri = dataRoot + f;
+
+	return fs.readFileAsync(uri, "utf8")
+	.then(CSVToArray)
+	.then(data => {
+		const trend = {};
+
+		data.forEach(r => {
+			trend[r[0]] = parseInt(r[1]);
+		});
+
+		return trend;
+	});
+}
+
+/*
 fs.readFileAsync(__dirname + "/data/multiTimeline.csv", "utf8")
 .then(CSVToArray)
 .then(data => {
@@ -36,3 +128,4 @@ fs.readFileAsync(__dirname + "/data/multiTimeline.csv", "utf8")
 	const json = JSON.stringify(dataset, null, "\t");
 	return fs.writeFileAsync(__dirname + "/dataset.json", json);
 });
+*/
--- generateNetwork.js
+++ generateNetwork.js
@@ -6,21 +6,23 @@ bluebird.promisifyAll(fs);
 fs.readFileAsync(__dirname + "/dataset.json", "utf8")
 .then(JSON.parse)
 .then(dataset => {
-	const network = new Architect.Perceptron(1, 3, 1);
+	const { records, labels, normals } = dataset;
+
+	const network = new Architect.Perceptron(3, 2, 1);
 	const trainer = new Trainer(network);
 
-	const trainingSet = dataset.slice(0, dataset.length / 2).map(record => {
-		return {
-			input: [ record.trend / 100 ],
-			output: [ record.value / 1000 ]
-		};
-	});
+	const trainingSet = records.slice(0, records.length / 1.5);
 
 	trainer.train(trainingSet, { iterations: 10 * 1000 });
 
-	network.activate([0]);
-	console.log(network.activate.toString());
+	fs.writeFileSync(__dirname + "/graph.csv", records.map(r => {
+		console.log(r);
+		return (r.output[0] * normals.output[0]) + "," + (network.activate(r.input)[0] * normals.output[0]);
+	}).join("\n"));
 
+	return network;
+})
+.then(network => {
 	const json = JSON.stringify(network.toJSON(), null, "\t");
 
 	fs.writeFileAsync(__dirname + "/net.json", json)
